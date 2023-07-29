2bb1d9759fcb43417f1e1317b557f31556d234d2
Joy <jor115@pitt.edu>
2023-07-29 00:57:09 -0400

    Site will inherit a dark mode view

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Still a work in progress
+
+// Function to toggle the dark mode styles
+function toggleDarkMode(e) {
+	if (e.matches) {
+	  document.body.classList.add('dark-mode');
+	} else {
+	  document.body.classList.remove('dark-mode');
+	}
+}
+
+// Watch for changes in the prefers-color-scheme media feature
+const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+darkModeQuery.addListener(toggleDarkMode);
+
+// Initial call to set the dark mode based on the user's system settings
+toggleDarkMode(darkModeQuery);
+
+
+
+
+
+
